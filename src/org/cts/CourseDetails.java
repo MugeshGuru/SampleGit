45236a35f221fdbcc5fcd094aab2de285297033a
MugeshGuru <mugeshguru29@gmail.com>
2022-01-10 14:19:25 +0530

    Done

---
+++ src/org/cts/CourseDetails.java
@@ -0,0 +1,32 @@
+package org.cts;
+
+public class CourseDetails {
+	
+	
+	// Method creation
+	private void javaCourse() {
+	
+		System.out.println("Java class");
+	}
+	
+
+	private void pythonCourse() {
+		
+		System.out.println("Python Class");
+	}
+	
+	public static void main(String[] args) {
+		
+		
+		//Classname objname = new Classname();-----> Object creation
+		
+		CourseDetails c = new CourseDetails();
+		
+		// Method calling
+		c.javaCourse();
+		c.pythonCourse();
+		
+	}
+	
+	
+}
